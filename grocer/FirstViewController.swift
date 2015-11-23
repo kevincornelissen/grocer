8e26dfc3e5c21e5c9966564a50739ec091a450a2
kevincornelissen <kevin@ichoosr.com>
2015-11-23 23:48:58 +0100

    init commit

--- grocer/FirstViewController.swift
+++ grocer/FirstViewController.swift
@@ -8,10 +8,43 @@
 
 import UIKit
 
-class FirstViewController: UIViewController {
+class FirstViewController: UIViewController, UITableViewDataSource {
+    let defaults = NSUserDefaults.standardUserDefaults()
 
+    
+    @IBOutlet weak var myTableView: UITableView!
+    
+    func tableView(tableView: UITableView, commitEditingStyle editingStyle: UITableViewCellEditingStyle, forRowAtIndexPath indexPath: NSIndexPath){
+    
+        if editingStyle == UITableViewCellEditingStyle.Delete{
+            myItemList.removeAtIndex(indexPath.row)
+            NSUserDefaults.standardUserDefaults().setObject(myItemList, forKey: "myItems")
+            myTableView.reloadData()
+        }
+    }
+
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return myItemList.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let myCell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "myCell")
+        myCell.textLabel?.text = myItemList[indexPath.row]
+        
+        return myCell
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        myTableView.reloadData()
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        if (NSUserDefaults.standardUserDefaults().objectForKey("myItems") != nil) {
+            myItemList = NSUserDefaults.standardUserDefaults().objectForKey("myItems") as! [String]
+        }
+        
+        
         // Do any additional setup after loading the view, typically from a nib.
     }
 
--- grocer/SecondViewController.swift
+++ grocer/SecondViewController.swift
@@ -8,8 +8,23 @@
 
 import UIKit
 
-class SecondViewController: UIViewController {
+var myItemList = [String]()
 
+
+class SecondViewController: UIViewController {
+    
+
+    @IBAction func doneEditing(sender: AnyObject) {
+        if myItemText.text?.characters.count > 0 {
+            myItemList.append(myItemText.text!)
+            myItemText.text = ""
+            self.view.endEditing(true)
+            NSUserDefaults.standardUserDefaults().setObject(myItemList, forKey: "myItems")
+        }
+    }
+    
+    @IBOutlet weak var myItemText: UITextField!
+        
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
